06ee913e4328a029648102aa86338029dacd58a6
Ely Kahn <ely.kahn@gmail.com>
2025-08-07 15:31:22 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    title: 'elyio', // Site title
+    author: 'ely kahn', // Author name
+    description: 'Simple place to write my thoughts', // Site description
     language: 'en-US' // Default language
   },
 
@@ -15,7 +15,7 @@ export const themeConfig: ThemeConfig = {
     contentWidth: '35rem', // Content area width
     centeredLayout: true, // Use centered layout (false for left-aligned)
     favicon: false, // Show favicon on index page
-    themeToggle: false, // Show theme toggle button (uses system theme by default)
+    themeToggle: true, // Show theme toggle button (uses system theme by default)
     footer: true, // Show footer
     fadeAnimation: true // Enable fade animations
   },
@@ -29,7 +29,7 @@ export const themeConfig: ThemeConfig = {
 
   // POST SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   post: {
-    readingTime: false, // Show reading time in posts
+    readingTime: true, // Show reading time in posts
     toc: true, // Show the table of contents (when there is enough page width)
     imageViewer: true, // Enable image viewer
     copyCode: true // Enable copy button in code blocks
